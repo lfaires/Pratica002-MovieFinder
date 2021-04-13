5910f1ebbc77c9ccfdedf9ca9796ebc4f4e45ad7
Luiz Fernando <lfaires@gmail.com>
2021-04-13 19:00:08 -0300

    função para enviar dados ao servidor

--- script/script.js
+++ script/script.js
@@ -13,10 +13,10 @@ function processarResposta(resposta){
     const filmes = resposta.data;
     console.log(filmes)
     for(let i=0;i<filmes.length;i++){
-        let filmografia = `<div class="movie">
+        let filmografia = `<div class="movie" id="${filmes[i].id}">
         <img src="${filmes[i].imagem}">
         <div class="title">${filmes[i].titulo}</div>
-        <button onclick="perguntarAoUsuario()">
+        <button onclick="enviarDados(${filmes[i].id})">
           Comprar
           <ion-icon name="cart-outline"></ion-icon>
         </button>
@@ -25,16 +25,13 @@ function processarResposta(resposta){
     }
 }
 
-function perguntarAoUsuario(){
+function enviarDados(numeroFilme){
     const nomeCliente = prompt("Qual o seu nome?")
     const qtdAssentos = prompt("Quantidade de assentos?")
     const pedido = {nome: nomeCliente, quantidade: qtdAssentos}
-    enviarDados(pedido)
-}
-
-function enviarDados(solicitacao){
-    const requisicao = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes/${idFilme}/ingresso`,solicitacao)
-
+    const requisicao = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes/${numeroFilme}/ingresso`,pedido)
+    console.log(numeroFilme)
+    console.log(nomeCliente)
 }
 
 function tratarErro(){
